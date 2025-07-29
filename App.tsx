37cf792aa5ba9dfe29f64521ab5c08557f313b89
chunhai2703 <oceanvo437@gmail.com>
2025-07-29 15:24:05 +0700

    Lesson 1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World with Ocean!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>Ocean nè!</Text>
+        <Text style={styles.parent}>
+          Welcome to <Text style={styles.child}>Ocean World</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>Hello World 1!</Text>
+      <Text>Hello World 2!</Text>
     </View>
   );
 }
@@ -16,5 +21,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  hello1: {
+    color: "red",
+    fontSize: 60,
+    borderColor: "blue",
+    borderWidth: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  parent: {
+    color: "green",
+    fontSize: 60,
+  },
+  child: {
+    color: "pink",
+    fontSize: 30,
   },
 });
