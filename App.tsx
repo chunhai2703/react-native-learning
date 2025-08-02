97efb5088112e1e47d0682f9e69883a1463f33b5
chunhai2703 <oceanvo437@gmail.com>
2025-08-02 14:24:59 +0700

    Using fonts in react native

--- App.tsx
+++ App.tsx
@@ -1,149 +1,32 @@
-import { useState } from "react";
-import {
-  Alert,
-  Button,
-  FlatList,
-  Keyboard,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
-import FlexBox from "./components/flexbox";
-import AntDesign from "@expo/vector-icons/AntDesign";
+import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import HomeScreen from "./components/review/home";
+import DetailScreen from "./components/review/detail";
+import AboutScreen from "./components/review/about";
 
-interface ITodo {
-  id: number;
-  name: string;
-}
-export default function App() {
-  const [todo, setTodo] = useState("");
-  const [listTodo, setListTodo] = useState<ITodo[]>([]);
+SplashScreen.preventAutoHideAsync();
 
-  function randomInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const handleDeleteTodo = (id: number) => {
-    const newTodos = listTodo.filter((item) => item.id !== id);
-    setListTodo(newTodos);
-  };
-
-  const handleAddToDo = () => {
-    if (!todo) {
-      Alert.alert("Lỗi input todo", "Todo không được để trống", [
-        // {
-        //   text: "Hủy",
-        //   onPress: () => console.log("Cancel Pressed"),
-        //   style: "cancel",
-        // },
-        { text: "Okay con dê", onPress: () => console.log("OK Pressed") },
-      ]);
-      return;
+const App = () => {
+  const [loaded, error] = useFonts({
+    "OpenSans-Regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
     }
-    setListTodo([...listTodo, { id: randomInteger(1, 2000000), name: todo }]);
-    setTodo("");
-  };
-
+  }, [loaded, error]);
+  if (!loaded && !error) {
+    return null;
+  }
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        {/* header */}
-        <Text style={styles.header}>Todo App</Text>
-
-        {/* form */}
-        <View style={styles.form}>
-          <TextInput
-            value={todo}
-            style={styles.todoInput}
-            onChangeText={(value) => setTodo(value)}
-          />
-          <Button title="Add todo" onPress={handleAddToDo} />
-        </View>
-
-        {/* list todo */}
-        <View style={styles.todo}>
-          <FlatList
-            data={listTodo}
-            keyExtractor={(item) => item.id + ""}
-            renderItem={({ item }) => {
-              return (
-                <Pressable
-                  style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1.0 }]}
-                  // onPress={() => handleDeleteTodo(item.id)}
-                >
-                  <View style={styles.groupTodo}>
-                    <Text style={styles.todoItem}>{item.name}</Text>
-                    <Pressable
-                      style={({ pressed }) => [
-                        { opacity: pressed ? 0.5 : 1.0 },
-                      ]}
-                      onPress={() => handleDeleteTodo(item.id)}
-                    >
-                      <Text>
-                        <AntDesign name="close" size={24} color="red" />
-                      </Text>
-                    </Pressable>
-                  </View>
-                </Pressable>
-              );
-            }}
-          />
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <View>
+      <HomeScreen />
+      <DetailScreen />
+      <AboutScreen />
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  header: {
-    backgroundColor: "orange",
-    paddingHorizontal: 20,
-    textAlign: "center",
-    fontSize: 60,
-  },
-
-  form: {
-    // flex: 2,
-    marginBottom: 20,
-  },
-  todo: {
-    flex: 1,
-  },
-
-  body: {
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  todoInput: {
-    borderBottomWidth: 1,
-    borderBottomColor: "green",
-    padding: 5,
-    margin: 15,
-  },
-
-  groupTodo: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    marginBottom: 15,
-    marginHorizontal: 15,
-    padding: 15,
-  },
-  todoItem: {
-    // padding: 10,
-    fontSize: 20,
-    // marginBottom: 20,
-  },
-});
+export default App;
